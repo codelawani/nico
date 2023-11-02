e5c2fe55839dfcc97b917e40387d0b4d4ed58a80
Nico <lawani.gk@gmail.com>
2023-11-02 14:36:38 +0100

    upd: add social links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 import { data } from "./site/config";
 export default function Home() {
+  const { github, linkedIn, x, email } = data.socials;
   return (
     <div className="mt-40 ml-10 mr-5">
       <div className="font-extrabold text-5xl">Hello I&apos;m Nico 👋</div>
-      <div className="mt-5">{data.about}</div>
+      <div className="mt-5">{data.summary}</div>
+      <div className="mt-5">
+        <div>
+          <Link className="text-blue-600" href={github.url}>
+            github{" "}
+          </Link>
+          <Link className="text-blue-600" href={x.url}>
+            x{" "}
+          </Link>
+          <Link className="text-blue-600" href={email.url}>
+            mail{" "}
+          </Link>
+          <Link className="text-blue-600" href={linkedIn.url}>
+            linkedIn{" "}
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/site/config.ts
+++ src/app/site/config.ts
@@ -1,3 +1,22 @@
 export const data = {
-  about: "Nico is a Backend Developer who loves to build fast, reliable, and secure web applications.",
-}
+  summary:
+    "Nico is a Backend Developer who loves to build fast ⚡, reliable 🦾 and secure 🔒 web applications.",
+  socials: {
+    github: {
+      name: "codelawani",
+      url: "https://github.com/codelawani",
+    },
+    linkedIn: {
+      name: "codelawani",
+      url: "https://www.linkedin.com/in/codelawani/",
+    },
+    x: {
+      name: "codelawani",
+      url: "https://x.com/codelawani",
+    },
+    email: {
+      name: "codelawani",
+      url: "mailto:codelawani@gmail.com",
+    },
+  },
+};
